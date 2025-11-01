6d5fc18c8d06134a62ee77b29895e30c3c1e62f4
develover <jajangsukmana222@gmail.com>
2025-11-02 02:25:49 +0700

    Add database connection logging for debugging

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -11,6 +12,9 @@ func ConnectDB() (*gorm.DB, error) {
 	dsn := "host=localhost user=postgres password=ithITtech dbname=akuntan port=5432 sslmode=disable"
 	if os.Getenv("DATABASE_URL") != "" {
 		dsn = os.Getenv("DATABASE_URL")
+		fmt.Printf("Using DATABASE_URL: %s\n", dsn)
+	} else {
+		fmt.Println("DATABASE_URL not set, using default localhost connection")
 	}
 	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
