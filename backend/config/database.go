b5c29a8093a9b944e4e479091b1fa381e503b404
sekumakers <jajangsukmana222@gmail.com>
2025-12-30 18:34:01 +0700

    Fix asset depreciation checkbox logic and bug in HitungPenyusutan.jsx

--- backend/config/database.go
+++ backend/config/database.go
@@ -9,7 +9,7 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "host=26.49.48.174 user=postgres password=12Maktab! dbname=akuntan port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=12Maktab! dbname=akuntan port=5432 sslmode=disable"
 	if os.Getenv("DATABASE_URL") != "" {
 		dsn = os.Getenv("DATABASE_URL")
 		fmt.Printf("Using DATABASE_URL: %s\n", dsn)
